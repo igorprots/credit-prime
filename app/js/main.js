dd9920559c61dbfb99d5958e90b5b3c4668bc0b4
Igor Protsenko <protsxe51@gmail.com>
2020-11-16 17:39:54 +0200

    add new block

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,16 @@
-$(function(){
+$(function () {
 
 
+    $(".areas__item").slice(0, 9).show();
+    $("#loadMore").click(function (e) {
+        e.preventDefault();
+        $(".areas__item:hidden").slice(0, 3).fadeIn("slow");
+        $(".areas__item").removeClass("areas__item--hide");
 
-  
+        if ($(".areas__item:hidden").length == 0) {
+            $("#loadMore").fadeOut("slow");
+        }
+    });
+
+
 });
